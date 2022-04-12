aa1f13be42af6487c11efdef1429e591aff60d09
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2022-04-12 17:08:10 -0500

    Added Camera

---
+++ stand_in_the_square/Camera/ICamera.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace stand_in_the_square.Camera
+{
+    /// <summary>
+    /// A 3D view camera
+    /// </summary>
+    interface ICamera
+    {
+        /// <summary>
+        /// The view matrix
+        /// </summary>
+        Matrix View { get; }
+
+        /// <summary>
+        /// The projection matrix
+        /// </summary>
+        Matrix Projection { get; }
+    }
+}
